5fbcdca349705a38f14c03511d5b6d0f7ee2a9d9
nir20ane <nirranjhane@gmail.com>
2019-01-11 16:07:29 -0800

    Update StringRotation_UsingContains.java

--- StringRotation_UsingContains.java
+++ StringRotation_UsingContains.java
@@ -3,7 +3,7 @@ of another.
 * Given two strings, S1 and S2, write code to check if S2 is a rotation of S1 using only one
 call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat")
 
-** Contenate string str1+str1 and check if str2 is in str1
+** Concatenate string str1+str1 and check if str2 is in str1
 * Time Complexity - O(n^2)
 * Space Complexity - O(n) - since we concatenate two strings
 */
